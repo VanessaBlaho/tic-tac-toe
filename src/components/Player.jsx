af2e69edfe6a6cb067ec17e8515fe6b7a31f0e78
Vanessa Blaho <vanessadblaho@gmail.com>
2024-01-10 16:44:35 +0100

    made name fields dynamic and possible to save

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,28 @@
+import { useState
+ } from "react";
+export default function Player ({initialName,symbol}){
+  const [playerName,setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+    function handleEditClick () {
+      setIsEditing((editing)=>!editing);
+    }
+    function handleChange(event) {
+      console.log(event);
+      setPlayerName(event.target.value);
+    }
 
-export default function Player ({name,symbol}){
-    return <li>
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
+    if(isEditing) {
+      editablePlayerName= <input type="text" required value={playerName} onChange={handleChange}/>;
+    }
+
+    return (
+    <li>
     <span className="player">
-      <span className="player-name">{name}</span>
+      {editablePlayerName}
       <span className="player-symbol">{symbol}</span>
     </span>
-    <button>Edit</button>
-    </li>;
+    <button onClick={handleEditClick}>{isEditing ? "Save":"Edit"}</button>
+    </li>
+    );
 }
