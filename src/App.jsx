c321c1337219f2f519ede5d91bff1ebb970dcf3a
Vanessa Blaho <vanessadblaho@gmail.com>
2024-01-15 16:44:03 +0100

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
+import Gameboard from "./components/Gameboard";
 import Player from "./components/Player";
+import { useState} from "react";
 
 function App() {
-  
+  const [activePlayer,setActivePlayer]= useState('X');
 
+  function handleSelectSquare(){
+    setActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X');
+  }
   return <main>
-      <div id="game-container">
+      <div id="game-container" className="highlight-player">
         <ol id="players">
         <Player
         initialName="Player 1"
         symbol="X"
+        isActive = {activePlayer === 'X'}
         />
            <Player
         initialName="Player 2"
         symbol="O"
+        isActive = {activePlayer === 'O'}
         />
         </ol>
-      
-
-      GAME BOARD
-      </div>
+      <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+        </div>
       LOG
       </main>;
   
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,9 @@
 import { useState
  } from "react";
-export default function Player ({initialName,symbol}){
+export default function Player ({initialName,symbol,isActive}){
   const [playerName,setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
-    
+
     function handleEditClick () {
       setIsEditing((editing)=>!editing);
     }
@@ -18,7 +18,7 @@ export default function Player ({initialName,symbol}){
     }
 
     return (
-    <li>
+    <li className={isActive ? 'active':undefined}>
     <span className="player">
       {editablePlayerName}
       <span className="player-symbol">{symbol}</span>
